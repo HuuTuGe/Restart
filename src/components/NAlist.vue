e232c5fdabd462ae55cd53bb66303e70d5cbfef3
q1ly <3353877295@qq.com>
2022-11-17 20:02:28 +0800

    普通成就+成就组件

---
+++ src/components/NAlist.vue
@@ -0,0 +1,30 @@
+<template>
+    <span class="cbox1">
+        {{ msg }}
+    </span>
+</template>
+
+<script lang="ts">
+
+export default {
+  name: 'NAlist',
+  data() {
+      return {
+    msg: ''       
+      }
+  },
+}
+</script>
+
+<style>
+.cbox1{
+    width: 160px;
+    height: 50px;
+    line-height: 40px;
+    background-color: rgb(131, 26, 113);
+    text-align: center;
+    font-size: 20px;
+    border: 1px solid rgba(187, 187, 187, 1);}
+    
+    
+</style>
