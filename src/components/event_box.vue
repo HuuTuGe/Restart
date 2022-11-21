16082fbb5965e6da19efc165c8bd3413baec8a35
HuuTuGe <1017636154@qq.com>
2022-11-21 19:29:34 +0800

    lq提交

---
+++ src/components/event_box.vue
@@ -0,0 +1,100 @@
+<template>
+  <div class="event_box">
+      <div class="term">{{mon}}</div>
+      <hr class="d_line">
+      <div class="con"><span class="con_in">{{Con_In}}</span></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'event_box',
+  data(){
+    return{
+      mon:'初始化',
+      Con_In:'初始化'
+    }
+  },
+  // methods:{
+  //   print_MON(){
+  //     this.con_id=1;
+  //     this.contant="八月上";
+  //     if(this.con_id==1){
+  //       this.mon=this.contant;
+  //     }
+  //   },
+  //   print_In(){
+  //     this.con_id=1;
+  //     this.con_='123456';
+  //     if(this.con_id==1){
+  //       this.Con_In=this.con_;
+  //     }
+  //   }
+  // },
+  mounted:function(){
+    // this.print_MON();
+    // this.print_In();
+  }
+
+}
+</script>
+
+<style scoped>
+.event_box{
+  width: 357px;
+  height: 75px;
+  border-style: solid;
+  border-color: #0050B3;
+  position: relative;
+  left: 15px;
+  margin-bottom: 10px;
+  margin-top: 15px;
+}
+.term{
+  position: relative;
+  display: inline-block;
+  width: 45px;
+  height: 45px;
+  font-size: 20px;
+  background-color: rgba(0,0,0,0);/*可修改为rgba(0,0,0,0) */
+  float: left;
+  left:15px ;
+  top:10px;
+  font-size: 20px;
+  text-align: left;
+  color:black;
+}
+.d_line{
+  position: relative;
+  display: inline-block;
+  float: left;
+  height: 75px;
+  border: none;
+  border-left: 4px dotted #0050B3;
+  top:-7px;
+  left: 25px;
+}
+.con{
+  position: relative;
+  display: inline-block;
+  float: left;
+  width: 285px;
+  height: 75px;
+  background-color:rgba(0,0,0,0);/*可修改为rgba(0,0,0,0) */
+  left: 24px;
+  color: black;
+  text-align: center;
+  vertical-align:middle;
+  /* line-height: 80px; */
+  word-wrap: break-word;
+  white-space:normal; 
+}
+.con_in{
+  display: inline-block;
+  width: 285px;
+  position: relative;
+  top: 50%;
+  transform: translateY(-50%);
+
+}
+</style>
--- src/components/play.vue
+++ src/components/play.vue
@@ -2,31 +2,49 @@
   <div class="app">
     <div class="header">
       <div class="head">
-      <!-- <div class="back"></div> -->
-      <return_box></return_box>
-      <div class="light"><img src="../assets/light.jpg" alt=""></img></div>
-      <h1 class="term">学期</h1>
-      <div class="shu">
-          <p class="head_p" style="color:white">属性值</p>
-          <hr noshade="true" style="color: white;">
-          <table>
-            <tr>
-              <td>智力：<span>6</span></td>
-              <td>体质：<span>6</span></td>
-            </tr>
-            <tr>
-              <td>魅力：<span>6</span></td>
-              <td>财富：<span>6</span></td>
-            </tr>
-            <tr>
-              <td>运气：<span>6</span></td>
-              <td>心情：<span>6</span></td>
-            </tr>
-          </table>
-          <span class="ach_p">已收集成就:</span>
-          <span class="ach_p">6</span>
+        <!-- <div class="back"></div> -->
+        <return_box></return_box>
+        <div class="light"><img src="../assets/light.jpg" alt=""></div>
+        <h1 class="term">学期</h1>
+        <div class="shu">
+            <p class="head_p" style="color:white">属性值</p>
+            <hr noshade="true" style="color: white;">
+            <div class="tab">
+              <table>
+                <tr>
+                  <!-- <td>智力：<span>6</span></td> -->
+                  <td><pro_item prop_name="智力" num=2></pro_item></td>
+                  <!-- <td>体质：<span>6</span></td> -->
+                  <td><pro_item prop_name="体质" num="6"></pro_item></td>
+                </tr>
+                <tr>
+                  <!-- <td>魅力：<span>6</span></td>
+                  <td>财富：<span>6</span></td> -->
+                  <td><pro_item prop_name="魅力" num="2"></pro_item></td>
+                  <td><pro_item prop_name="财富" num="6"></pro_item></td>
+                </tr>
+                <tr>
+                  <!-- <td>运气：<span>6</span></td>
+                  <td>心情：<span>6</span></td> -->
+                  <td><pro_item prop_name="运气" num="1"></pro_item></td>
+                  <td><pro_item prop_name="心情" num="6"></pro_item></td>
+                </tr>
+                <tr>
+                  <td colspan="2"><pro_item prop_name="已收集成就" num="6"></pro_item></td>
+                </tr>
+              </table>
+              <!-- <span class="ach_p">已收集成就:</span>
+              <span class="ach_p">6</span> -->
+            </div>
+            
+        </div>
       </div>
     </div>
+    <div class="con" @click="print_div">
+        <event_box></event_box>
+        <ach_box></ach_box>
+        <event_box></event_box>
+        <event_box></event_box>
     </div>
     
   </div>
@@ -35,12 +53,18 @@
 <script>
 import { provide } from 'vue';
 import return_box from './return_box.vue'
+import pro_item from './pro_item.vue'
+import event_box from './event_box.vue'
+import ach_box from './ach_box'
 
   export default{
-    name: "play",
+    name: 'play',
     components: { 
       provide,
-      return_box
+      return_box,
+      pro_item,
+      event_box,
+      ach_box
      }
 }
 </script>
@@ -60,6 +84,27 @@ div{
   -moz-border-radius: 10px;
   -webkit-border-radius: 10px;
   -o-border-radius: 10px;
+}
+
+.header{
+  position: relative;
+  background-color:white;
+  height: 312px;
+}
+
+.head{
+  background-color:#BAE7FF;
+  height: 250px;
+  width: 390px;
+  position: relative;
+}
+
+.con{
+  position:absolute;
+  background-color: white;
+  top:330px;
+  width: 390px;
+  height: 400px;
 }
 
 .back{
@@ -85,19 +130,6 @@ div{
   /* -o-transform: rotate(45deg);
   -moz-transform: rotate(45deg); */
 }
-
-.header{
-  position: relative;
-  background-color:white;
-  height: 312px;
-}
-
-.head{
-  background-color:#BAE7FF;
-  height: 250px;
-  width: 390px;
-  position: relative;
-}
 .shu{
   position:relative;
   top: px;
@@ -109,6 +141,7 @@ div{
   width: 345px;
 }
 
+
 .head_p{
   width: 58px;
   height: 172;
@@ -118,7 +151,7 @@ div{
   vertical-align: middle;
   position: relative;
   top: -56px;
-  left: -8px;
+  left: -120px;
 }
 .ach_p{
   display: inline-block;
@@ -130,22 +163,30 @@ div{
   top: -45%;
   left: 2%;
 }
+.tab{
+  position:relative;
+  display: inline-block;
+  top:-100px;
+  height:240px;
+  width: 340px;
+}
 
 table{
+  text-align: left;
   display: inline;
-  border-collapse: 10px;
   color: white;
   display: inline-block;
   font-size: 20px;
   margin: 0px;
   position: relative;
-  top: -17px;
-  left: -10px;
+  top: -100px;
+  left: 30px;
+  border-collapse: separate;
 }
 td{
-  display: inline-block;
-  padding-left:40px;
   padding-bottom: 10px;
+  padding-left: 30px;
+  width: 100px;
 }
 h1{
   display: inline-block;
@@ -161,6 +202,7 @@ hr{
   margin: 0;
   position: relative;
   top:10%;
+  left: -35%;
   background-color: white;
 }
 img{
