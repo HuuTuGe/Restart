30f695a8c4f24cde8dc25939b4f3520de67d03da
XZhang785 <752646336@qq.com>
2022-11-21 20:39:59 +0800

    feat: 更新了首页跳转天赋选择页面的路由

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,13 @@
 import Vue from 'vue'
 import VueRouter, { RouteConfig } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import index from '../components/index.vue'
-import HelloWorld from '../components/HelloWorld.vue'
+import index from '../views/IndexView.vue'
+import SelectTalent from  '../views/SelectTalentView.vue'
+import Success from '../views/SuccessView.vue'
+import NAchievement from '../views/NAchievementView.vue'
+import SAchievement from '../views/SAchievementView.vue' 
+import AchievementList from '../views/AchievementListView.vue'
+import summary from '../components/summary.vue'
+import play from '../components/play.vue'
 
 Vue.use(VueRouter)
 
@@ -13,26 +18,48 @@ const routes: Array<RouteConfig> = [
     component: index
   },
   {
-    path: '/hello',
-    name: 'hello',
-    component: HelloWorld
+    path: '/select_talent',
+    name: 'select-talent',
+    component: SelectTalent
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    path: '/success',
+    name: 'success',
+    component: Success
+  },
+  {
+    path: '/summary',
+    name: 'summary',
+    component: summary
+  },
+  {
+    path: '/NAchievementList',
+    name: 'NAchievementList',
+    component: NAchievement
+  },
+  {
+    path: '/SAchievement',
+    name: 'SAchievement',
+    component: SAchievement
+  },
+  {
+    path: '/AchievementList',
+    name: 'AchievementList',
+    component: AchievementList
   },
   {
     path: '*', redirect: '/',
     name: 'redirected'
+  },
+  {
+    path: '/play',
+    name: 'play',
+    component: play
   }
 ]
 
 const router = new VueRouter({
-  routes
+  routes,
 })
 
 export default router
---
+++ src/views/IndexView.vue
@@ -0,0 +1,120 @@
+<template>
+    <div id="background">
+        <div class="upper_list">
+            <img src="@/assets/sound.png" class="sound">
+            <img src="@/assets/change.png" class="sound">
+            <img src="@/assets/prize.jpg" class="prize">
+        </div>
+        <div class="test_list">
+            <h1 class="word">成就</h1>
+        </div>
+        <img src="@/assets/RESTART@1x.png" class='restart'/>
+        <img src='@/assets/线条@1x.png' class='line'/>
+        <img src="@/assets/福大人生@1x.png" class='life'/>
+        <router-link to="/select_talent">
+            <img src="@/assets/play.png" class="play">
+        </router-link>
+        
+        <index></index>
+    </div>
+</template>
+<script lang="ts"> 
+export default {
+    name: 'app',
+    data() {
+        return {
+            English_name: 'RESTART',
+            Chinese_name: '福大人生',
+        }
+    }
+
+}
+</script>
+  
+<style scoped lang="scss">
+
+.line{
+    left: 41px;
+    top: 360px;
+    width: 310px;
+    height: NaNpx;
+    border: 9px solid rgba(149, 29, 29, 1);
+}
+.app {
+    width: 390px;
+    height: 722px;
+    border: 1px solid #000;
+    text-align: center;
+    margin: 0 auto;
+    background-color: #f7f7f7;
+}
+
+.upper_list {
+    width: 390px;
+    height: 85px;
+}
+
+.test_list {
+    width: 390px;
+    height: 10px;
+}
+
+body {
+    font-family: ”Microsoft YaHei”, Arial, Helvetica, sans-serif, ”宋体”;
+}
+
+.prize {
+    padding-left: 150px;
+    padding-top: 15px;
+    width: 108px;
+    height: 85px;
+    float: left;
+}
+
+.sound {
+    padding-left: 20px;
+    padding-top: 15px;
+    width: 40px;
+    height: 40px;
+    float: left;
+}
+
+
+.play {
+    text-align: center;
+    padding-top: 80px;
+    width: 178px;
+    height: 72px;
+}
+
+.word {
+    padding-top: 0px;
+    padding-left: 255px;
+    font-size: 24px;
+}
+
+.restart {
+    padding-left: 10px;
+    padding-top: 110px;
+    padding-bottom: 5px;
+    width: 297px;
+    height: 77px;
+    color: rgba(149, 29, 29, 1);
+    font-size: 72px;
+    text-align: left;
+    font-family: Microsoft YaHei;
+}
+
+.life {
+    width: 200px;
+    height: 74px;
+    color: rgba(149, 29, 29, 1);
+    font-size: 48px;
+    padding-left: 30px;
+    padding-top: 5px;
+    font-family: YouYuan;
+    font-weight: bold
+}
+</style>
+  
+  
