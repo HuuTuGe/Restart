700c3423886720ca10eb9201f03f492c57c52275
HuuTuGe <1017636154@qq.com>
2022-11-24 19:10:27 +0800

    首页字的位置修改和鼠标移入按钮放大

--- src/views/IndexView.vue
+++ src/views/IndexView.vue
@@ -10,18 +10,18 @@
         <div class="test_list">
             <h1 class="word">成就</h1>
         </div>
-        
-        <img src="@/assets/RESTART@1x.png" class='restart'/>
-        <img src='@/assets/线条@1x.png' class='line'/>
-        <img src="@/assets/福大人生@1x.png" class='life'/>
+
+        <img src="@/assets/RESTART@1x.png" class='restart' />
+        <img src='@/assets/线条@1x.png' class='line' />
+        <img src="@/assets/福大人生@1x.png" class='life' />
         <router-link to="/Choice">
             <img src="@/assets/play.png" class="play">
         </router-link>
-        
+
         <index></index>
     </div>
 </template>
-<script lang="ts"> 
+<script lang="ts">
 export default {
     name: 'app',
     data() {
@@ -35,14 +35,14 @@ export default {
 </script>
   
 <style scoped lang="scss">
-
-.line{
+.line {
     left: 41px;
     top: 360px;
     width: 310px;
     height: NaNpx;
     border: 9px solid rgba(149, 29, 29, 1);
 }
+
 .app {
     width: 390px;
     height: 722px;
@@ -88,6 +88,23 @@ body {
     padding-top: 80px;
     width: 178px;
     height: 72px;
+    display: inline-block;
+    vertical-align: middle;
+    -webkit-transform: perspective(1px) translateZ(0);
+    transform: perspective(1px) translateZ(0);
+    box-shadow: 0 0 1px rgba(0, 0, 0, 0);
+    -webkit-transition-duration: 0.3s;
+    transition-duration: 0.3s;
+    -webkit-transition-property: box-shadow, transform;
+    transition-property: box-shadow, transform;
+}
+
+.play:hover,
+.play:focus,
+.play:active {
+    box-shadow: 0 10px 10px -10px rgba(0, 0, 0, 0.5);
+    -webkit-transform: scale(1.1);
+    transform: scale(1.1);
 }
 
 .word {
@@ -113,7 +130,7 @@ body {
     height: 74px;
     color: rgba(149, 29, 29, 1);
     font-size: 48px;
-    padding-left: 30px;
+    padding-left: 15px;
     padding-top: 5px;
     font-family: YouYuan;
     font-weight: bold
