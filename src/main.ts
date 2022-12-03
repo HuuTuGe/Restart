d97783f40f721bdabd7f05bbfb92e10207ef5a22
XZhang785 <752646336@qq.com>
2022-12-03 19:26:04 +0800

    填加状态

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import axios from 'axios'
+import { createPinia, PiniaVuePlugin } from 'pinia'
+
+Vue.use(PiniaVuePlugin)
+const pinia = createPinia()
 
 Vue.prototype.$axios = axios
 Vue.config.productionTip = false
 new Vue({
   router,
+  pinia,
   render: h => h(App)
 }).$mount('#app')
