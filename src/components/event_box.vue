7dcaff8b7cadec5d9e6f41188f6e6c029aa860d1
lqqisoutofpower <2651209638@qq.com>
2022-12-06 19:58:58 +0800

    ach_box,event_box,play.vue,letter.png

--- src/components/event_box.vue
+++ src/components/event_box.vue
@@ -1,52 +1,133 @@
 <template>
-  <div class="event_box">
+  <div>
+    <div class="event_box">
       <div class="term">{{mon}}</div>
       <hr class="d_line">
       <div class="con"><span class="con_in">{{Con_In}}</span></div>
+    </div>
+    <div id="ach" v-if="(ach_id == true)">
+      <ach_box></ach_box>
+    </div>
   </div>
+  
 </template>
 
-<script>
-export default {
+
+<script lang="ts">
+// import  play from './play.vue'
+import Vue from 'vue';
+import axios from 'axios'
+import ach_box from './ach_box.vue'
+import {defineEmits} from 'vue'
+
+export default Vue.extend({
+  
   name: 'event_box',
+  props:['cDay'],
   data(){
     return{
-      mon:'初始化',
-      Con_In:'初始化'
+      mon:'',
+      Con_In:'',
+      ach_id:false,
+      cday:this.cDay
     }
   },
-  // methods:{
-  //   print_MON(){
-  //     this.con_id=1;
-  //     this.contant="八月上";
-  //     if(this.con_id==1){
-  //       this.mon=this.contant;
-  //     }
-  //   },
-  //   print_In(){
-  //     this.con_id=1;
-  //     this.con_='123456';
-  //     if(this.con_id==1){
-  //       this.Con_In=this.con_;
-  //     }
-  //   }
-  // },
+  components:{
+    ach_box
+  },
   mounted:function(){
-    // this.print_MON();
-    // this.print_In();
-  }
-
-}
+    this.print_MON();
+    this.print_In();
+  },
+  methods:{
+      print_In(){
+      let _this=this
+      axios.get('https://mock.apifox.cn/m1/1984536-0-default/event')//url
+      .then(response =>{
+        _this.Con_In = response.data.lifeEvent.content;//事件id
+        _this.ach_id = response.data.haveAchievement;
+        
+      })
+    },
+    print_MON(){
+      if(this.cday==17){
+        this.mon="九月上"
+        console.log(this.cday)
+      }else if(this.cday==16){
+        this.mon="九月下"
+        console.log(this.cday)
+      }
+      else if(this.cday==15){
+        this.mon="十月上"
+        console.log(this.cday)
+      }else if(this.cday==14){
+        this.mon="十月下"
+        console.log(this.cday)
+      }else if(this.cday==13){
+        this.mon="十一月上"
+        console.log(this.cday)
+      }else if(this.cday==12){
+        this.mon="十一月下"
+        console.log(this.cday)
+      }else if(this.cday==11){
+        this.mon="十二月上"
+        console.log(this.cday)
+      }else if(this.cday==10){
+        this.mon="十二月下"
+        console.log(this.cday)
+      }else if(this.cday==9){
+        this.mon="一月上"
+        console.log(this.cday)
+      }else if(this.cday==8){
+        this.mon="三月上"
+        console.log(this.cday)
+      }else if(this.cday==7){
+        this.mon="三月下"
+        console.log(this.cday)
+      }else if(this.cday==6){
+        this.mon="四月上"
+        console.log(this.cday)
+      }else if(this.cday==5){
+        this.mon="四月下"
+        console.log(this.cday)
+      }else if(this.cday==4){
+        this.mon="五月上"
+        console.log(this.cday)
+      }else if(this.cday==3){
+        this.mon="五月下"
+        console.log(this.cday)
+      }else if(this.cday==2){
+        this.mon="六月上"
+        console.log(this.cday)
+      }else if(this.cday==1){
+        this.mon="六月下"
+        console.log(this.cday)
+      }else if(this.cday==0){
+        this.mon="七月上"
+        console.log(this.cday)
+      }
+      // this.mon="八月上"
+      // console.log(this.cday)
+    }
+  },
+  
+})
 </script>
 
 <style scoped>
+div {
+  border-radius: 10px;
+  -moz-border-radius: 10px;
+  -webkit-border-radius: 10px;
+  -o-border-radius: 10px;
+}
 .event_box{
   width: 357px;
   height: 75px;
   border-style: solid;
   border-color: #0050B3;
   position: relative;
-  left: 15px;
+  left: 10px;
   margin-bottom: 10px;
   margin-top: 15px;
 }
